005ba2d60266a420951d7eb603390fd2b3af565c
Tajya <tajya@gmail.com>
2021-04-20 22:27:28 +0100

    shop page start

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/homepage.component.jsx';
-
-const HatsPage1 = (props) => {
-  console.log(props);
-  return (<div>
-    <button onClick={() => props.history.push('/')}>Home</button>
-    <h1>Hats Page</h1>
-    <Link to={`${props.match.url}/13`}>13</Link>
-    <br></br>
-    <Link to={`${props.match.url}/24`}>24</Link>
-    <br></br>
-    <Link to={`${props.match.url}/3`}>3</Link>
-  </div>
-);
-  }
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats1' component={HatsPage1} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
